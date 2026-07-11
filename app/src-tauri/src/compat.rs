9e9557da7aa0822be50b9c0b98d9ac131b1c1688
Mark <7296488+markzyu@users.noreply.github.com>
2026-07-11 16:38:56 -0700

    chore: bundle public dir into app assets

---
+++ app/src-tauri/src/compat.rs
@@ -0,0 +1,80 @@
+use std::path::PathBuf;
+use tauri::{AppHandle, Manager};
+use tauri_plugin_fs::FsExt;
+use tauri_plugin_os::family;
+
+pub(crate) fn get_compat_os_data_dir(app_handle: &AppHandle) -> Result<PathBuf, String> {
+  let base_dir = app_handle
+    .path()
+    .app_data_dir()
+    .map_err(|e| e.to_string())?;
+  let public_dir_desktop = base_dir.join("public");
+  let public_dir_android = base_dir.join("files/public");
+  match family() {
+    "linux" => Ok(public_dir_desktop),
+    "windows" => Ok(public_dir_desktop),
+    "macos" => Ok(public_dir_desktop),
+    "android" => Ok(public_dir_android),
+    _ => Err(format!("Unsupported operating system: {}", family())),
+  }
+}
+
+pub(crate) fn ensure_os_data_dir(app_handle: &AppHandle) -> Result<PathBuf, String> {
+  let public_dir = get_compat_os_data_dir(app_handle)?;
+  if !public_dir.exists() {
+    std::fs::create_dir_all(&public_dir).map_err(|e| e.to_string())?;
+  }
+  Ok(public_dir)
+}
+
+pub(crate) fn ensure_templates_dir(
+  app_handle: &AppHandle,
+  template_scope: &str,
+) -> Result<PathBuf, String> {
+  let public_dir = get_compat_os_data_dir(app_handle)?;
+  let template_dir = public_dir.join("templates").join(template_scope);
+  if !template_dir.exists() {
+    std::fs::create_dir_all(&template_dir).map_err(|e| e.to_string())?;
+  }
+  Ok(template_dir)
+}
+
+pub(crate) fn ensure_instances_dir(
+  app_handle: &AppHandle,
+  instance_id: String,
+  relative_path: Option<String>,
+) -> Result<PathBuf, String> {
+  let public_dir = get_compat_os_data_dir(app_handle)?;
+  let mut instances_dir = public_dir.join("instances").join(instance_id);
+  if let Some(relative_path) = relative_path {
+    instances_dir = instances_dir.join(relative_path);
+  }
+  if !instances_dir.exists() {
+    std::fs::create_dir_all(&instances_dir).map_err(|e| e.to_string())?;
+  }
+  Ok(instances_dir)
+}
+
+pub(crate) fn read_app_asset(
+  app_handle: &AppHandle,
+  relative_path: String,
+) -> Result<Vec<u8>, String> {
+  let resource_dir_uri = app_handle
+    .path()
+    .resource_dir()
+    .map_err(|e| e.to_string())?;
+  let resource_uri = resource_dir_uri.join("prefabs").join(relative_path);
+  app_handle
+    .fs()
+    .read(resource_uri)
+    .map_err(|e| e.to_string())
+}
+
+pub(crate) fn copy_app_asset_to_fs(
+  app_handle: &AppHandle,
+  relative_path: String,
+  target_path: &PathBuf,
+) -> Result<(), String> {
+  let asset_bytes = read_app_asset(app_handle, relative_path)?;
+  std::fs::write(&target_path, asset_bytes).map_err(|e| e.to_string())
+}
--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,11 +1,68 @@
+mod compat;
+
 use std::path::PathBuf;
 
 use lcdn_server::{AppState, LcdnConfig, LcdnStatus};
+use tauri::AppHandle;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
   format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+async fn get_template_install_dir(
+  app_handle: AppHandle,
+  template_scope: &str,
+  template_name: &str,
+) -> Result<PathBuf, String> {
+  let public_path = compat::ensure_templates_dir(&app_handle, template_scope)?;
+  let public_path = public_path.join(format!("{}.zip", template_name));
+  let template_asset_path = format!("templates/{}/{}.zip", template_scope, template_name);
+  compat::copy_app_asset_to_fs(&app_handle, template_asset_path, &public_path)?;
+  Ok(public_path)
+}
+
+const EXAMPLE_INSTANCE_ID: &'static str = "6fa27a2f-2f1e-413d-a842-424242424242";
+
+#[tauri::command]
+async fn get_prefab_instance_install_dir(
+  app_handle: AppHandle,
+  instance_id: String,
+) -> Result<PathBuf, String> {
+  let public_dir_path = compat::ensure_instances_dir(&app_handle, instance_id.clone(), None)?;
+  let content_dir_path = compat::ensure_instances_dir(
+    &app_handle,
+    instance_id.clone(),
+    Some("content".to_string()),
+  )?;
+  let assets_dir_path =
+    compat::ensure_instances_dir(&app_handle, instance_id.clone(), Some("assets".to_string()))?;
+  compat::copy_app_asset_to_fs(
+    &app_handle,
+    format!("instances/{}/instance.json", instance_id),
+    &public_dir_path.join("instance.json"),
+  )?;
+  compat::copy_app_asset_to_fs(
+    &app_handle,
+    format!("instances/{}/content/main.en.json", instance_id),
+    &content_dir_path.join("main.en.json"),
+  )?;
+  compat::copy_app_asset_to_fs(
+    &app_handle,
+    format!("instances/{}/assets/hero.jpg", instance_id),
+    &assets_dir_path.join("hero.jpg"),
+  )?;
+  Ok(public_dir_path)
+}
+
+#[tauri::command]
+async fn perform_first_time_setup(app_handle: AppHandle) -> Result<(), String> {
+  compat::ensure_os_data_dir(&app_handle)?;
+  get_template_install_dir(app_handle.clone(), "@tcms", "template-example-info-card1").await?;
+  get_prefab_instance_install_dir(app_handle, EXAMPLE_INSTANCE_ID.to_string()).await?;
+  Ok(())
 }
 
 #[tauri::command]
@@ -42,10 +99,14 @@ pub fn run() {
   lcdn_server::setup_rustls();
 
   tauri::Builder::default()
+    .plugin(tauri_plugin_fs::init())
     .plugin(tauri_plugin_os::init())
     .plugin(tauri_plugin_opener::init())
     .invoke_handler(tauri::generate_handler![
+      get_prefab_instance_install_dir,
+      get_template_install_dir,
       greet,
+      perform_first_time_setup,
       lcdn_start,
       lcdn_stop,
       lcdn_reload_configs,
