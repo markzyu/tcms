750d28aac7f54aff4a24e99d113014dbca87f9b8
Mark <7296488+markzyu@users.noreply.github.com>
2026-08-17 13:06:49 -0700

    feat: basic interactions and score tracking

--- templates/game-dropnclick/src/GameCanvas.tsx
+++ templates/game-dropnclick/src/GameCanvas.tsx
@@ -17,9 +17,12 @@ type GameEntity = {
   textStyle: TextStyle;
   drop: Drop;
   dropVariant?: Variant;
+
+  wasClicked?: boolean;
 }
 
-const TICK_INTERVAL = 2000;
+const TICK_INTERVAL = 200;
+const SPAWN_INTERVAL = 1000;
 
 export const getRandomDrop = (gameConfig: GameConfig) => {
   const { drops } = gameConfig;
@@ -33,28 +36,43 @@ export const getRandomDrop = (gameConfig: GameConfig) => {
 
 export type GameLoopProps = {
   gameConfig: GameConfig;
+  tick: number;
   setEntities: (_callback: (entities: GameEntity[]) => GameEntity[]) => void;
+  setScore: (_callback: (score: number) => number) => void;
 }
 
 const speedX = (Math.random() - 0.5) * 100;
 const speedY = (Math.random() - 0.5) * 100;
 
-export const gameLoopTick = ({ gameConfig, setEntities }: GameLoopProps) => {
+export const gameLoopTick = (props: GameLoopProps) => {
+  const { gameConfig, tick, setEntities, setScore } = props;
   const { drop, variant } = getRandomDrop(gameConfig);
   const startX = Math.random() * window.innerWidth * 3 / 4;
   const startY = Math.random() * window.innerHeight * 3 / 4;
   const currTime = Date.now();
   setEntities((entities) => {
-    return [...entities, {
-      id: uuidv4(),
-      startX,
-      startY,
-      startTime: Date.now(),
-      text: variant?.name || drop.baseName,
-      textStyle: variant?.textStyle || drop.baseTextStyle,
-      drop,
-      dropVariant: variant,
-    }].filter((entity) => {
+    const SPWAN_TICK = SPAWN_INTERVAL / TICK_INTERVAL;
+    let deltaScore = 0;
+    const newEntities = entities.filter((entity) => {
+      if (entity.wasClicked) {
+        deltaScore += 1;
+      }
+      return !entity.wasClicked;
+    });
+    setScore((score) => score + deltaScore);
+    if (tick % SPWAN_TICK === 0) {
+      newEntities.push({
+        id: uuidv4(),
+        startX,
+        startY,
+        startTime: Date.now(),
+        text: variant?.name || drop.baseName,
+        textStyle: variant?.textStyle || drop.baseTextStyle,
+        drop,
+        dropVariant: variant,
+      });
+    }
+    return newEntities.filter((entity) => {
       const movementX = speedX * (currTime - entity.startTime) / 1000;
       const movementY = speedY * (currTime - entity.startTime) / 1000;
       return (
@@ -75,27 +93,47 @@ declare module "react" {
 export const GameCanvas = () => {
   const { contentJson, isLoading } = usePageContentContext<GameConfig>();
   const [entities, setEntities] = useState<GameEntity[]>([]);
+  const [score, setScore] = useState(0);
   if (!contentJson || isLoading) return "Loading...";
 
   useEffect(() => {
+    let tick = 0;
     setInterval(() => {
-      gameLoopTick({ gameConfig: contentJson, setEntities });
+      gameLoopTick({ gameConfig: contentJson, tick, setEntities, setScore });
+      tick++;
     }, TICK_INTERVAL);
-  }, [contentJson, setEntities]);
+  }, [contentJson, setEntities, setScore]);
+
+  const onClickEntity = (entity: GameEntity) => () => {
+    setEntities((entities) => {
+      return entities.map((e) => {
+        if (e.id === entity.id) {
+          return { ...e, wasClicked: true };
+        }
+        return e;
+      });
+    });
+  };
 
   return (
     <div
       className="bg-blue-500 w-full h-full overflow-hidden"
       data-testid={TEST_IDS.gameRoot}
     >
+      <div className="absolute top-0 left-0 w-full bg-red-500">Score: {score}</div>
       {entities.map((entity) => (
-        <div key={entity.id} className="absolute select-none" style={{
-          left: entity.startX,
-          top: entity.startY,
-          animation: `movement 100s linear`,
-          '--speed-x-for-100-secs': `${speedX * 100}px`,
-          '--speed-y-for-100-secs': `${speedY * 100}px`,
-        }}>
+        <div
+          key={entity.id}
+          className={`absolute select-none ${entity.wasClicked ? 'bg-green-500' : ''}`}
+          style={{
+            left: entity.startX,
+            top: entity.startY,
+            animation: `movement 100s linear`,
+            '--speed-x-for-100-secs': `${speedX * 100}px`,
+            '--speed-y-for-100-secs': `${speedY * 100}px`,
+          }}
+          onClick={onClickEntity(entity)}
+        >
           {entity.text}
         </div>
       ))}
