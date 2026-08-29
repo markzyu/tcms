3f12554defbfe0a6041967f63d174e6523494772
Mark <7296488+markzyu@users.noreply.github.com>
2026-08-29 14:47:02 -0700

    feat: editing slug, content w/o debug ui

--- app/src/admin/home/InstanceCard.vue
+++ app/src/admin/home/InstanceCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { IonButton, IonCard, IonCardContent, IonIcon, IonSpinner, IonList, IonItem, IonInput, IonTextarea } from '@ionic/vue';
-  import { alertCircle } from 'ionicons/icons';
+  import { alertCircle, createOutline } from 'ionicons/icons';
   import { computed, onUnmounted, ref, watch } from 'vue';
   import { useEditableInstanceConfigs, useLocalCDNControls } from './hooks.ts';
   import { useAdminHomePageContent } from './content.ts';
@@ -24,6 +24,7 @@
 
   const showErrorTooltip = ref(false);
   const urlToVisit = ref<string | null>(null);
+  const editStarted = ref(false);
 
   const {
     isLocalCDNRunning,
@@ -66,6 +67,7 @@
   const { startWorkflow } = useWorkflow();
   const openEditWorkflow = async () => {
     try {
+      editStarted.value = false;
       const schemaString = await invokeWithType(z.string(), 'read_template_schema', {
         templateScope: '@tcms',
         templateName: 'template-example-info-card1',
@@ -96,12 +98,36 @@
   </div>
   <ion-card class="w-full mx-auto max-w-[500px]" data-testid="instance-card">
     <div class="relative h-[220px] rounded-t-sm overflow-hidden">
-      <iframe data-testid="preview-iframe" class="absolute left-0 top-0 w-full object-cover max-w-none h-full -z-10" :src="iframeSrc" alt="Random image" referrerpolicy="no-referrer" />
+      <div v-if="editStarted" class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <ion-button fill="outline" color="light" @click="openEditWorkflow">
+          <ion-icon slow="icon-only" :icon="createOutline" aria-hidden="true" />
+        </ion-button>
+      </div>
+      <iframe
+        data-testid="preview-iframe"
+        :class="{
+          'absolute left-0 top-0 w-full object-cover max-w-none h-full -z-10': true,
+          'brightness-50': editStarted,
+        }"
+        :src="iframeSrc"
+        alt="Random image"
+        referrerpolicy="no-referrer"
+      />
     </div>
     <ion-card-content class="-mb-2" data-testid="instance-status">
-      Test: {{ urlSlug }} {{ isLocalCDNRunning ? '(Running)' : '' }} {{ localCDNError ? '(Error)' : '' }}
+      <ion-input
+        v-if="editStarted"
+        class="-my-2"
+        fill="outline"
+        type="text"
+        data-testid="url-slug-input"
+        v-model="urlSlug"
+      />
+      <div v-else class="h-[40px]">
+        Test: {{ urlSlug }} {{ isLocalCDNRunning ? '(Running)' : '' }} {{ localCDNError ? '(Error)' : '' }}
+      </div>
     </ion-card-content>
-    <div class="flex justify-end gap-2">
+    <div class="flex justify-end gap-2 mt-2">
       <ion-button size="small" fill="clear" data-testid="start-cdn-button" @click="urlSlug && startLocalCDN(urlSlug)" v-if="!isLocalCDNRunning">
         <ion-spinner class="w-4 h-4 mr-2" v-if="isLocalCDNStarting"></ion-spinner>
         <ion-icon class="w-4 h-4 mr-1 fill-red-600" :icon="alertCircle" v-if="localCDNError"></ion-icon>
@@ -114,12 +140,12 @@
       <div class="pointer-events-none absolute bottom-10 left-20 right-20 bg-white p-2 rounded-md shadow-md" v-if="localCDNError && showErrorTooltip">
         {{ localCDNError }}
       </div>
-      <ion-button size="small" fill="clear" data-testid="edit-button" @click="openEditWorkflow">{{ instanceEditBtnLabel }}</ion-button>
+      <ion-button size="small" fill="clear" data-testid="edit-button" @click="editStarted = !editStarted">{{ instanceEditBtnLabel }}</ion-button>
       <ion-button size="small" fill="clear" data-testid="share-button">{{ instanceShareBtnLabel }}</ion-button>
     </div>
   </ion-card>
-  <ion-card class="w-full mx-auto max-w-[500px] lg:max-w-none pr-2" data-testid="instance-debug-card">
-    <ion-card-content class="" data-testid="debug-tools-heading">
+  <ion-card class="hidden w-full mx-auto max-w-[500px] lg:max-w-none pr-2" data-testid="instance-debug-card">
+    <ion-card-content data-testid="debug-tools-heading">
       {{ debugToolsHeading }}
     </ion-card-content>
     <ion-list>
